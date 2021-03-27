f6bb1dac80d59d28c6fa10b432132e473939402a
pradipta <pradipta26@gmail.com>
2021-03-27 12:23:16 -0400

    Intermediate Commit 1.

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,10 +1,19 @@
+import java.util.List;
+
 public class FastCollinearPoints {
+    private int numberOfSegments = 0;
+    private List<Point[]> segmentList;
     public FastCollinearPoints(Point[] points) {   // finds all line segments containing 4 or more points
     }
     public           int numberOfSegments() {       // the number of line segments
         return 0;
     }
     public LineSegment[] segments() {                // the line segments
-        
+        LineSegment[] lineSegment = new LineSegment[segmentList.size()];
+        int index = 0;
+        for (Point[] segment : segmentList) {
+            lineSegment[index++] = new LineSegment(segment[0], segment[1]);
+        }
+        return lineSegment;
     }
 }
